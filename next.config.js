5565148b0005cdc767f067647881695a7a30b8bb
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2022-07-16 06:37:02 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
 		loader: "akamai",
 		path: "",
 	},
-	basePath: "/alexribeirodesa-site",
-	assetPrefix: "/alexribeirodesa-site",
+	basePath: "",
+	assetPrefix: "",
 }
 
 module.exports = nextConfig
